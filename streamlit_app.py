9d942d146f1496f62df62ef5ef4f14662456dcf0
jyotirmayapatro <156374618+jyotirmayapatro@users.noreply.github.com>
2024-10-22 02:49:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def generate_response(prompt, max_length, temperature):
         inputs,
         max_length=max_length,
         temperature=temperature,
-        num_return_sequences=2
+        num_return_sequences=1
     )
  
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
