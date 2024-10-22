a03b2ed26f14b052337aa64f75aa67c4ac04c189
jyotirmayapatro <156374618+jyotirmayapatro@users.noreply.github.com>
2024-10-22 02:49:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import torch
 model = GPT2LMHeadModel.from_pretrained("gpt2")
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
-# Ref: https://github.com/huggingface/transformers/blob/main/examples/pytorch/text-generation/run_generation.py
+
 # creating a function to generate response from the GPT-2 model
 def generate_response(prompt, max_length, temperature):
     inputs = tokenizer.encode(prompt, return_tensors='pt')
@@ -23,18 +23,16 @@ def generate_response(prompt, max_length, temperature):
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
 ### App details ### 
-##Ref: https://github.com/dataprofessor/streamlit-freecodecamp-course/blob/main/app.py
 # App name
 st.title("JP's GPT-2 Text Generation App")
 
 # user prompt input
-prompt = st.text_input("Enter your prompt:")
+prompt = st.text_input("Enter your prompt:", value="Once upon a time")
 
 # token details
-token_length = st.number_input("Number of tokens to generate:", min_value=10, max_value=150, value=50)
+token_length = st.number_input("Number of tokens to generate:", min_value=3, max_value=150, value=30)
 
 # button
-# Ref: https://docs.streamlit.io
 if st.button("Generate Responses"):
     # # # high temperature/creative
     st.subheader("Creative Response :")
